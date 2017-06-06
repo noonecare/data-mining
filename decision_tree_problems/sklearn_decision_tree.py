72416a803ef7e27c1a01e73020ef8aeb4d23b0e8
王蒙 <wangmeng@gridsum.com>
2017-06-06 21:03:49 +0800

    用 sklearn 对于 watermelon1.csv 中的数据建立决策树模型，模型保存在 tree.dot 和 tree.png 中。 tree.png 是由 tree.dot 生成的。现在必须手动在 tree.dot 中声明 fontname="FangSong" 否则中文没法正确显示

---
+++ decision_tree_problems/sklearn_decision_tree.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+print(__doc__)
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier
+
+from sklearn import tree
+
+tree.export_graphviz
+
